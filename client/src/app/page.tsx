209716ce39175712453bb64d69d64eb0ebb0ef53
Pham Trung Hieu <trunghieupham03@gmail.com>
2023-09-29 19:43:31 +0700

    Refactor

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,3 @@
-import { TbAirConditioning } from "react-icons/tb";
-import { AiOutlineRight } from "react-icons/ai";
-import { Block_Devices } from "@/components/Block_Devices";
-
-
 export default function Home() {
-  return <main>
-    <Block_Devices name="Air Conditioner" backgroundColor="purple" Icon={<TbAirConditioning size={24} />} />
-  </main>;
+  return <main></main>;
 }
