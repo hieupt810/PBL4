2a2e3f96a7af83c2004f12e3ccd15787c8ae43e3
ThaiKhacDuoc <khacduockhamduc@gmail.com>
2023-10-05 15:43:26 +0700

    Fix blocl_Devices

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { TbAirConditioning } from "react-icons/tb";
-import { AiOutlineRight } from "react-icons/ai";
 import { Block_Devices } from "@/components/Block_Devices";
 
-
 export default function Home() {
-  return <main>
-    <Block_Devices name="Air Conditioner" backgroundColor="purple" Icon={<TbAirConditioning size={24} />} />
-  </main>;
+  return (
+    <main>
+      <Block_Devices
+        name="Air Condition"
+        backgroundColor="purple"
+        Icon={<TbAirConditioning size={24} />}
+      />
+    </main>
+  );
 }
--- client/src/components/Block_Devices.tsx
+++ client/src/components/Block_Devices.tsx
@@ -1,25 +1,37 @@
 import { ReactNode } from "react";
-import { AiOutlineRight } from "react-icons/ai";
 import { twMerge } from "tailwind-merge";
+import { AiOutlineRight } from "react-icons/ai";
 
 interface Block_DevicesProps {
   name: string;
   backgroundColor: string;
-  Icon: ReactNode; 
+  Icon: ReactNode;
+}
+
+function getClassesForColor(color: string) {
+  const colorMap = {
+    purple: {
+      bg: 'bg-purple-600',
+      hover: 'hover:bg-purple-700'
+    }
+  };
+
+  return (colorMap as any) [color] || {};
 }
 
 export function Block_Devices({ name, backgroundColor, Icon }: Block_DevicesProps) {
+  const colorClasses = getClassesForColor(backgroundColor);
   return (
     <div
-      className={twMerge(
-        `bg-${backgroundColor}-600 p-4 rounded-[15px] h-24 text-white mt-6 ml-2 w-40 hover:bg-${backgroundColor}-700 flex flex-col justify-center`
-      )}
+    className={twMerge(
+      `${colorClasses.bg} p-4 rounded-[15px] h-24 text-white mt-6 ml-2 w-40 ${colorClasses.hover} flex flex-col justify-center`
+    )}
     >
       <div className="flex items-center w-full justify-between mb-2.5">
-        {Icon}
+        <strong>{Icon}</strong>
         <AiOutlineRight className="mr-2.0" />
       </div>
-        <span className="text-base ml-2">{name}</span>
+        <strong className="text-lg ml-2">{name}</strong>
     </div>
   );
 }
